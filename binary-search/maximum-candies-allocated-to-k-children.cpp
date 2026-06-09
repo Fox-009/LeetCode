a37c0969bd31ec0f20c5c823598ab869fb77f25e
SHIVAM <shivamsrivastava@jklu.edu.in>
2026-06-10 01:57:19 +0530

    LC-maximum-candies-allocated-to-k-children [Runtime: N/A; Memory: N/A]

---
+++ binary-search/maximum-candies-allocated-to-k-children.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    long long f(vector<int>&candies,int mid){
+        long long total = 0;
+        for( int candy : candies){
+            total += candy/mid;
+        }
+        return total;
+    }
+    int maximumCandies(vector<int>& candies, long long k) {
+        if (k>candies.size()) return 0;
+        int low = 1;
+        int high = *max_element(candies.begin(),candies.end());
+        while(low<= high){
+            int mid = low + (high-low)/2;
+            long long val = f(candies,mid);
+            if(val>=k){
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
+            }
+        }
+        return high;
+    }
+};
