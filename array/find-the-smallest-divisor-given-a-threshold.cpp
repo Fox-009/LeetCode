80e7ed826438fc2fe3580e41aeb507da0294cce2
SHIVAM <shivamsrivastava@jklu.edu.in>
2026-06-10 12:30:20 +0530

    LC-find-the-smallest-divisor-given-a-threshold [Runtime: N/A; Memory: N/A]

---
+++ array/find-the-smallest-divisor-given-a-threshold.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool possible(vector<int>&nums,int mid,int threshold){
+        int cnt = 0;
+        for( int it : nums){
+            cnt += (it+mid-1)/mid;
+        }
+        if (cnt <= threshold) return true;
+        else return false;
+    }
+    int smallestDivisor(vector<int>& nums, int threshold) {
+        int low = 0;
+        int high = *max_element(nums.begin(),nums.end());
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(possible(nums,mid,threshold)){
+                high = mid - 1;
+            }
+            else{
+                low = mid +1;
+            }
+        }
+        return low;
+    }
+};
