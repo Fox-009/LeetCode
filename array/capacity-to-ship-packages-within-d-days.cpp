3d00101ce4f1f3c2039c4ca0ddef07ce61005922
SHIVAM <shivamsrivastava@jklu.edu.in>
2026-06-10 13:39:04 +0530

    LC-capacity-to-ship-packages-within-d-days [Runtime: N/A; Memory: N/A]

--- array/capacity-to-ship-packages-within-d-days.cpp
+++ array/capacity-to-ship-packages-within-d-days.cpp
@@ -1,45 +1,33 @@
-#include <vector>
-#include <algorithm>
-using namespace std;
-
 class Solution {
 public:
-    bool canShip(const vector<int>& weights,int days,int capacity) 
-    {
-        int usedDays=1;
-        int currentLoad=0;
+    bool Ispossible(vector<int>& nums, long long weight, int days) {
+    long long total = 0;
+    int day = 1;
 
-        for(int w : weights)
-        {
-            if(currentLoad+w>capacity) 
-            {
-                usedDays++;
-                currentLoad=0;
-            }
-            currentLoad+=w;
+    for (int it : nums) {
+        if (total + it > weight) {
+            day++;
+            total = it;
+
+            if (day > days)
+                return false;
+        } else {
+            total += it;
         }
-
-        return usedDays<=days;
     }
 
+    return true;
+}
     int shipWithinDays(vector<int>& weights, int days) {
-        int left=*max_element(weights.begin(),weights.end());
-        int right=0;
-        for(int w : weights) 
-            right+=w;
-
-        while(left<right) 
-        {
-            int mid=left+(right-left)/2;
-            if(canShip(weights,days,mid)) 
-            {
-                right=mid;
-            } 
-            else
-            {
-                left=mid+1;
+        long long low = *max_element(weights.begin(),weights.end());
+        long long high = accumulate(weights.begin(), weights.end(), 0);
+        while(low <= high){
+            long long mid = low + (high -low)/2;
+            if (Ispossible(weights,mid,days)){
+                high = mid - 1;
             }
+            else low = mid+1;
         }
-        return left;
+        return low;
     }
 };
