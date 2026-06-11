3eb9837b86c1639c461be018d82e50c4ea615cf1
SHIVAM <shivamsrivastava@jklu.edu.in>
2026-06-11 21:18:31 +0530

    LC-remove-outermost-parentheses [Runtime: N/A; Memory: N/A]

---
+++ stack/remove-outermost-parentheses.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        int cnt = 0;
+        string ans;
+        for(char ch : s){
+            if (ch == ')'){
+                cnt--;
+            }
+            if(cnt>0) ans+=ch;
+            if (ch == '('){
+                cnt++;
+            }
+        }
+        return ans;   
+    }
+};
