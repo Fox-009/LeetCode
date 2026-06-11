6dc96a4801956dc513e935a79e4bbb9dbff5c0bd
SHIVAM <shivamsrivastava@jklu.edu.in>
2026-06-12 00:40:05 +0530

    LC-longest-common-prefix [Runtime: N/A; Memory: N/A]

--- trie/longest-common-prefix.cpp
+++ trie/longest-common-prefix.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& arr) {
-        if(arr.empty()) return "";
-        string ans;
-        for(int i = 0; i<arr[0].size();i++){
-            char ch = arr[0][i];
-            for(int j = 1;j<arr.size();j++ ){
-                if (i>= arr[j].size() || ch != arr[j][i]){
-                    return ans;
-                }
-            }
-            ans+=ch;
+    sort(arr.begin(),arr.end());
+    string ans;
+    for(int i = 0; i<arr[0].size();i++){
+        char ch = arr[0][i];
+        if(i>= arr[arr.size()-1].size() || ch != arr[arr.size()-1][i]){
+            return ans;
         }
-        return ans;
+        else{
+            ans += ch;
+        }
+    }
+    return ans;
     }
 };
