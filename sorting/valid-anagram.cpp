3bcc5711f1b6b23ab9747fecd4d6b167eb86f822
SHIVAM <shivamsrivastava@jklu.edu.in>
2026-06-13 01:58:31 +0530

    LC-valid-anagram [Runtime: N/A; Memory: N/A]

--- sorting/valid-anagram.cpp
+++ sorting/valid-anagram.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        if(s.size() != t.size()) return false;
-        vector<int>check(258,0);
-        for(int i = 0; i<s.size();i++){
-            check[s[i]]++;
-        }
-        for(int i = 0; i<s.size();i++){
-            if(check[s[i]] != check[t[i]]) return false;
-        }
-        return true;
+        if (s.size() != t.size()) return false;
+
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+
+        return s == t;
     }
 };
