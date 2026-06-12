d4ba494c5d6e5b92f0e66444b1eca7daeb9c98b6
SHIVAM <shivamsrivastava@jklu.edu.in>
2026-06-13 02:07:08 +0530

    LC-valid-anagram [Runtime: N/A; Memory: N/A]

--- hash-table/valid-anagram.cpp
+++ hash-table/valid-anagram.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if (s.size() != t.size()) return false;
-
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-
-        return s == t;
+        vector<int>v1(258,0);
+        vector<int>v2(258,0);
+        if(s.size() != t.size()) return false;
+        for(int i = 0; i<s.size();i++){
+            v1[(unsigned char)s[i]]++;
+            v2[(unsigned char)t[i]]++;
+        }
+        return v1 == v2;
     }
 };
