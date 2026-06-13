b233f1afe492ffba928b1afe224213a77c2520b7
SHIVAM <shivamsrivastava@jklu.edu.in>
2026-06-13 21:59:44 +0530

    LC-set-mismatch [Runtime: N/A; Memory: N/A]

--- bit-manipulation/set-mismatch.cpp
+++ bit-manipulation/set-mismatch.cpp
@@ -6,11 +6,12 @@ public:
         int missing ;
         for(int i = 0; i<nums.size();i++){
             mpp[nums[i]]++;
+        }
+        for(int i = 1; i<=nums.size();i++){
+            
             if (mpp[i] == 2){
                 duplicate = i;
             }
-        }
-        for(int i = 1; i<=nums.size();i++){
             if (mpp[i] == 0){
                 missing = i;
                 break;
