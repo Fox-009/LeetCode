9bbec0c1bc73821692e910e057535a6570c59022
SHIVAM <shivamsrivastava@jklu.edu.in>
2026-06-13 19:15:17 +0530

    LC-shuffle-the-array [Runtime: N/A; Memory: N/A]

--- array/shuffle-the-array.cpp
+++ array/shuffle-the-array.cpp
@@ -1,23 +1,11 @@
 class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
-        vector<int>x;
-        vector<int>y;
-        for(int i = 0; i< n ; i++){
-            x.push_back(nums[i]);
-        }
-        for(int i = n; i<nums.size() ; i++){
-            y.push_back(nums[i]);
-        }
-        int i  = 0;
-        int j = 0;
-        vector<int>v1;
-        while (i<x.size() && j< y.size()){
-            v1.push_back(x[i]);
-            i++;
-            v1.push_back(y[j]);
-            j++;
-        }
-        return v1;
+       vector<int>suffled(2*n,0);
+       for(int i = 0;i< n;i++){
+        suffled[2*i] = nums[i];
+        suffled[2*i + 1] = nums[n+i];
+       }
+       return suffled;
     }
 };
