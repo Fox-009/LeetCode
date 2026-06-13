58415d381aafb48662ab791cb5aaf082aff1d405
SHIVAM <shivamsrivastava@jklu.edu.in>
2026-06-13 20:21:27 +0530

    LC-set-mismatch [Runtime: N/A; Memory: N/A]

--- hash-table/set-mismatch.cpp
+++ hash-table/set-mismatch.cpp
@@ -1,11 +1,20 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
+        map<int,int>mpp;
         vector<int>v1;
         sort(nums.begin(),nums.end());
         for(int i = 0; i<nums.size();i++){
-            if(nums[i] != (i+1)){
-                v1.push_back(nums[i]);
+            mpp[nums[i]]++;
+        }
+        int duplicate ;
+        for(auto it : mpp){
+            if (it.second == 2){
+                v1.push_back(it.first);
+            }
+        }
+        for(int i = 0; i<nums.size();i++){
+            if (nums[i] != i+1){
                 v1.push_back(i+1);
             }
         }
