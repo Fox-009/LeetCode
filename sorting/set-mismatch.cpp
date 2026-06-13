81acd72ee037bfb66515aaa6cbb6adb878546c14
SHIVAM <shivamsrivastava@jklu.edu.in>
2026-06-13 20:36:43 +0530

    LC-set-mismatch [Runtime: N/A; Memory: N/A]

--- sorting/set-mismatch.cpp
+++ sorting/set-mismatch.cpp
@@ -2,17 +2,18 @@ class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
         map<int,int>mpp;
-        for(int i = 0; i<nums.size();i++){
-            mpp[nums[i]]++;
-        }
         int duplicate ;
         int missing ;
+        for(int i = 0; i<nums.size();i++){
+            mpp[nums[i]]++;
+            if (mpp[i] == 2){
+                duplicate = i;
+            }
+        }
         for(int i = 1; i<=nums.size();i++){
             if (mpp[i] == 0){
                 missing = i;
-            }
-            if (mpp[i] == 2){
-                duplicate = i;
+                break;
             }
         }
         return {duplicate,missing};
