70ba5b578df3f73464dd172c9fc24b99001663da
SHIVAM <shivamsrivastava@jklu.edu.in>
2026-06-13 20:13:13 +0530

    LC-set-mismatch [Runtime: N/A; Memory: N/A]

--- bit-manipulation/set-mismatch.cpp
+++ bit-manipulation/set-mismatch.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
         vector<int>v1;
+        sort(nums.begin(),nums.end());
         for(int i = 0; i<nums.size();i++){
             if(nums[i] != (i+1)){
                 v1.push_back(nums[i]);
