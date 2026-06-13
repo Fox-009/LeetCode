0af5763fa22c8c59c0b46afb5ec14419873a0220
SHIVAM <shivamsrivastava@jklu.edu.in>
2026-06-13 22:08:52 +0530

    LC-how-many-numbers-are-smaller-than-the-current-number [Runtime: N/A; Memory: N/A]

---
+++ array/how-many-numbers-are-smaller-than-the-current-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+    vector<int>v1;
+    for(int i = 0; i<nums.size();i++){
+        int count = 0;
+        for(int j = 0; j<nums.size();j++){
+            if(nums[i]>nums[j]){
+                count++;
+            }
+        }
+        v1.push_back(count);
+        }
+        return v1; 
+    }
+};
