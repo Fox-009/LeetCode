686f2b7e6c20de54889ee5a6de7ba676c8dd2c67
SHIVAM <shivamsrivastava@jklu.edu.in>
2026-06-14 09:02:16 +0530

    LC-find-all-numbers-disappeared-in-an-array [Runtime: N/A; Memory: N/A]

--- array/find-all-numbers-disappeared-in-an-array.cpp
+++ array/find-all-numbers-disappeared-in-an-array.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
-        vector<int>missing;
-        map<int,int>mp;
-        for(int i = 0; i<nums.size();i++){
-            mp[nums[i]]++;
-        }
-        for(int i = 1; i<=nums.size();i++){
-            if (mp[i] == 0){
-                missing.push_back(i);
+        vector<int> ans;
+        for(int i = 0; i<nums.size(); i++){
+            int index = abs(nums[i])-1;
+            if(nums[index]>0){
+                nums[index]*=-1;
             }
         }
-        return missing;
+        for(int i = 0; i<nums.size();i++){
+            if(nums[i]>0){
+                ans.push_back(i+1);
+            }
+        }
+        return ans;
     }
 };
