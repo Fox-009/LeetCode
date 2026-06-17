5dbed07b956d1d1945510c8a889a1604e91868f5
SHIVAM <shivamsrivastava@jklu.edu.in>
2026-06-17 23:57:46 +0530

    LC-contains-duplicate [Runtime: N/A; Memory: N/A]

--- array/contains-duplicate.cpp
+++ array/contains-duplicate.cpp
@@ -3,11 +3,11 @@ public:
     bool containsDuplicate(vector<int>& nums) {
         unordered_set<int> hash;
 
-        for(int i = 0; i < nums.size(); i++) {
-            if(hash.find(nums[i]) != hash.end())
+        for(auto x : nums) {
+            if(hash.find(x) != hash.end())
                 return true;
 
-            hash.insert(nums[i]);
+            hash.insert(x);
         }
         return false;
     }
