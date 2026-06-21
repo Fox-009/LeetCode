78567fa8e2686fa5c308988ff0f8a2b8889f75a7
SHIVAM <shivamsrivastava@jklu.edu.in>
2026-06-21 22:23:28 +0530

    LC-split-array-largest-sum [Runtime: N/A; Memory: N/A]

--- prefix-sum/split-array-largest-sum.cpp
+++ prefix-sum/split-array-largest-sum.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
-    bool Ispossible(vector<int>&nums,long long mid,int k){
-        long long total = 0;
+    bool Ispossible(int mid,  vector<int>&nums, int k){
         int s = 1;
+        int total = 0;
         for(int it : nums){
-            if(total + it > mid){
+            if (total + it > mid){
                 s++;
-                if(s>k) return false;
+                if( s > k) return false;
                 total = it;
             }
             else{
                 total+=it;
             }
         }
-        return true;
+        return s<= k;
     }
+    
     int splitArray(vector<int>& nums, int k) {
-        int low = *max_element(nums.begin(),nums.end());
-        long long high = accumulate(nums.begin(),nums.end(),0LL);
-        while(low <= high){
-            int mid = low+(high-low)/2;
-            if(Ispossible(nums,mid,k)){
+    int low = *max_element(nums.begin(),nums.end());
+    long long high = accumulate(nums.begin(),nums.end(),0LL);
+    while( low <= high ){
+        long mid = low +(high - low )/2;
+            if (Ispossible(mid,nums,k)){
                 high = mid - 1;
             }
             else{
-                low = mid +1;
+                low = mid + 1;
             }
         }
         return low;
