2b53dfea50fb5a37d7d9c00db4d895204b493b21
SHIVAM <shivamsrivastava@jklu.edu.in>
2026-07-11 15:47:36 +0530

    LC-squares-of-a-sorted-array [Runtime: N/A; Memory: N/A]

--- two-pointers/squares-of-a-sorted-array.cpp
+++ two-pointers/squares-of-a-sorted-array.cpp
@@ -3,11 +3,9 @@ public:
     vector<int> sortedSquares(vector<int>& nums) {
         int n = nums.size();
         vector<int> ans(n);
-
         int left = 0;
         int right = n - 1;
         int idx = n - 1;
-
         while (left <= right) {
             int leftSquare = nums[left] * nums[left];
             int rightSquare = nums[right] * nums[right];
@@ -20,7 +18,6 @@ public:
                 right--;
             }
         }
-
         return ans;
     }
 };
