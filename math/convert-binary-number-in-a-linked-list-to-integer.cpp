581a7faf0080ad0e9e22ff0def6269586fdef887
SHIVAM <shivamsrivastava@jklu.edu.in>
2026-08-10 20:38:45 +0530

    LC-convert-binary-number-in-a-linked-list-to-integer [Runtime: N/A; Memory: N/A]

---
+++ math/convert-binary-number-in-a-linked-list-to-integer.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int getDecimalValue(ListNode* head) {
+        int result = 0;
+        ListNode* Current = head;
+        while(Current != NULL){
+            result = result*2+Current->val;
+            Current = Current->next;
+        }
+        return result;
+    }
+};
