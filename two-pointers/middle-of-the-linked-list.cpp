e46cddcdf89cdab6a739b5fa3cdd32e841584710
SHIVAM <shivamsrivastava@jklu.edu.in>
2026-08-10 20:53:21 +0530

    LC-middle-of-the-linked-list [Runtime: N/A; Memory: N/A]

--- two-pointers/middle-of-the-linked-list.cpp
+++ two-pointers/middle-of-the-linked-list.cpp
@@ -13,15 +13,9 @@ public:
     ListNode* middleNode(ListNode* head) {
         ListNode* slow = head;
         ListNode* fast = head;
-        int count= 0;
-        
         while (fast != NULL && fast->next != NULL) {
             slow = slow->next;
             fast = fast->next->next;
-            count +=2;
-        }
-        if (count/2 == 0){
-            return slow->next;
         }
         return slow;
     }
