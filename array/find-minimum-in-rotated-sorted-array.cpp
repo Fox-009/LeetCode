87d2dd316290d8472f8eea4ced1d4486f17c4439
SHIVAM <shivamsrivastava@jklu.edu.in>
2026-08-16 20:12:29 +0530

    LC-find-minimum-in-rotated-sorted-array [Runtime: N/A; Memory: N/A]

--- array/find-minimum-in-rotated-sorted-array.cpp
+++ array/find-minimum-in-rotated-sorted-array.cpp
@@ -7,14 +7,16 @@ public:
         while (low <= high){
             int mid = low + (high - low)/2;
 
-            if (nums[low] < nums[high]) return nums[low];
-            else if (nums[low] < nums[high]) {
-                target = min(target, nums[low]);
-                break;
+            if (nums[low] < nums[high]) return min(target,nums[low]);
+            else if (nums[low] <= nums[mid]){
+                target = min(target,nums[low]);
+                low = mid + 1;
             }
             else{
-                target = min(target, nums[mid]);
-                high = mid - 1;
+                if (nums[mid] <= nums[high]){
+                    target = min(target, nums[mid]);
+                    high = mid-1;
+                }
             }
         }
         return target;
